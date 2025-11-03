42993d2b5f4435d04eff7af50227ad6c47c0849d
Anna Ovcharenko <anyutka1426@gmail.com>
2025-11-03 18:11:55 +0200

    change CafeInfo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "../Cafeinfo/CafeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
