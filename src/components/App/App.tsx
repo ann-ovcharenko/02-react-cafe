4abc3fdbc9c7bfa77f1a00a0ee451ccaaa898815
Anna Ovcharenko <anyutka1426@gmail.com>
2025-11-04 10:27:45 +0200

    fix2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import React, { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
